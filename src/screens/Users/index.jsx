7c02293aae0ff6aea41c536797a38bfe82fabc3a
Nickolas lira7 <125027693+Nickolaslira7@users.noreply.github.com>
2024-03-30 13:10:36 -0300

    Update Users index.jsx

--- src/screens/Users/index.jsx
+++ src/screens/Users/index.jsx
@@ -1,11 +1,13 @@
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
 
 import styles from "./styles";
 import Title from "../../components/Title";
 import TouchButton from "../../components/TouchButton";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Users({ route }) {
   const { data } = route.params;
+  const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
@@ -27,6 +29,9 @@ export default function Users({ route }) {
         <Text style={styles.planetComunicacao}>{data.comunicacao}</Text>
         <Text style={styles.planetGovernante}>{data.governante}</Text>
       </View>
+      <TouchableOpacity onPress={() => navigation.navigate("Planets", data)}>
+        <Text style={styles.planetButton}>Edit</Text>
+      </TouchableOpacity>
     </View>
   );
 }
